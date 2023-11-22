7472e3cda626932b838ece5650b789fc9ebf15ab
ErikSaft <143608501+ErikSaft@users.noreply.github.com>
2023-11-22 11:53:33 +0100

    Update reaksjonsspel.js

--- js/reaksjonsspel.js
+++ js/reaksjonsspel.js
@@ -1,31 +1,36 @@
-let startTime;
-let endTime;
-let readyButton = document.getElementById('readyButton');
-let resetButton = document.getElementById('resetButton');
-let clickBox = document.getElementById('clickBox');
+window.onload = function() {
+    let startTime;
+    let endTime;
+    let isClickable = false;
+    let readyButton = document.getElementById('readyButton');
+    let resetButton = document.getElementById('resetButton');
+    let clickBox = document.getElementById('clickBox');
 
-readyButton.addEventListener('click', function() {
-    this.disabled = true;
-    setTimeout(makeGreen, Math.random() * 5000);
-});
+    readyButton.addEventListener('click', function() {
+        this.disabled = true;
+        isClickable = true;
+        setTimeout(makeGreen, Math.random() * 5000);
+    });
 
-resetButton.addEventListener('click', function() {
-    clickBox.style.backgroundColor = 'red';
-    document.getElementById('reactionTime').innerHTML = "";
-    readyButton.disabled = false;
-});
-
-clickBox.addEventListener('click', function() {
-    if (this.style.backgroundColor == 'green') {
-        endTime = new Date();
-        let reactionTime = (endTime - startTime) / 1000;
-        document.getElementById('reactionTime').innerHTML = "Din reaksjonstid er: " + reactionTime + " sekunder";
-        this.style.backgroundColor = 'red';
+    resetButton.addEventListener('click', function() {
+        clickBox.style.backgroundColor = 'red';
+        document.getElementById('Reaksjonstid').innerHTML = "";
         readyButton.disabled = false;
-    }
-});
+    });
 
-function makeGreen() {
-    clickBox.style.backgroundColor = 'green';
-    startTime = new Date();
+    clickBox.addEventListener('click', function() {
+        if (this.style.backgroundColor == 'green' && isClickable) {
+            endTime = new Date();
+            let reactionTime = (endTime - startTime) / 1000;
+            document.getElementById('reactionTime').innerHTML = "Din reaksjonstid er: " + reactionTime + " sekunder";
+            this.style.backgroundColor = 'red';
+            readyButton.disabled = false;
+            isClickable = false;
+        }
+    });
+
+    function makeGreen() {
+        clickBox.style.backgroundColor = 'green';
+        startTime = new Date();
+    }
 }
