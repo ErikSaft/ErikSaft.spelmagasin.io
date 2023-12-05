6ffce2e862c4ee1178915d4404f3bff6410865dd
ErikSaft <143608501+ErikSaft@users.noreply.github.com>
2023-12-05 14:52:33 +0100

    op

--- js/steinspel.js
+++ js/steinspel.js
@@ -28,7 +28,7 @@ function datamaskinTurn(){
             break;
     }
 }
-//Skjekker kven som vant
+//Sjekker kven som vant
 function checkWinner(){
     if(spelar == datamaskin){
         return "Uavgjort!"; // Står uavgjort om begge velger det samme
